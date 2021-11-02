588be7e7c8a0d5805663c86e8d8fefbc46264995
UnusualAbsurd <nottherealabsurdchad@gmail.com>
2021-11-02 18:54:50 +0700

    + ModLog Function

--- functions/guild.js
+++ functions/guild.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const modLog = require('../data/models/ModLog')
 
 module.exports = {
     /**
@@ -13,7 +14,19 @@ module.exports = {
     successMsg: (interaction, message) => {
       return interaction.reply({ embeds: [new Discord.MessageEmbed().setColor("GREEN").setDescription(`<:greentick:905011429529235467> ${message}`)], ephemeral: false})
     },
-    modLog: async(guildId, embed) => {
-      
+    modLog: async(interaction, guildID, embed, row) => {
+       const data = await modLog.findOne({ guildID });
+       if(!data) return;
+       if(data) {
+         const channel = interaction.guild.channels.cache.get(data.channelID);
+         if(!channel) return;
+         if(channel) {
+          let components;
+          if(!row) components = []; 
+          if(row) components = [row];
+           
+           channel.send({ embeds: [embed], components })
+         }
+       }
     }
 }
